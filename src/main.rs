0d91959e55127e78f661c4603bffbf1bbfc4500c
raimon <raimon49@hotmail.com>
2022-09-07 19:18:16 +0900

    Add a code about call 'strlen' function from C library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    {
+        use std::os::raw::c_char;
+        use std::ffi::CString;
+
+        extern {
+            fn strlen(s: *const c_char) -> usize;
+        }
+
+        let rust_str = "I'll be back";
+        let null_terminated = CString::new(rust_str).unwrap();
+        unsafe {
+            assert_eq!(strlen(null_terminated.as_ptr()), 12);
+        }
+    }
 }
