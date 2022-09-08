15f2c4dfe289f65df1e55e9072384fcb16f67952
raimon <raimon49@hotmail.com>
2022-09-08 18:45:17 +0900

    Add an annotate comment about call 'strlen' function from C library

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,17 @@ fn main() {
         use std::os::raw::c_char;
         use std::ffi::CString;
 
+        // Rustの実行ファイルとリンクされるCライブラリで定義されている関数を宣言
         extern {
             fn strlen(s: *const c_char) -> usize;
         }
 
         let rust_str = "I'll be back";
-        let null_terminated = CString::new(rust_str).unwrap();
+        let null_terminated = CString::new(rust_str).unwrap(); // NULL文字で終端されたCの文字列を作る（引数にNULLが含まれているとCの文字列は作成できずエラーとなるためunwrapで取り出す必要がある）
+
+        // externブロック内で宣言された関数を呼ぶ時はunsafeとして扱われる
         unsafe {
+            // as_ptr()は文字列の先頭を *const c_char で返すため strlen() に渡せる
             assert_eq!(strlen(null_terminated.as_ptr()), 12);
         }
     }
