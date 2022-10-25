e6a794b61215a3d34f0c4b178766cc08f9517998
raimon <raimon49@hotmail.com>
2022-10-25 18:54:31 +0900

    Move types from git/raw to git/mod

---
+++ src/git/mod.rs
@@ -0,0 +1,22 @@
+use std::error;
+use std::fmt;
+use std::result;
+
+#[derive(Debug)]
+pub struct Error {
+    code: i32,
+    message: String,
+    class: i32
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> result::Result<(), fmt::Error> {
+        self.message.fmt(f)
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str { &self.message }
+}
+
+pub type Result<T> = result::Result<T, Error>;
--- src/git/raw.rs
+++ src/git/raw.rs
@@ -1,28 +1,6 @@
 #![allow(non_camel_case_types)]
 
 use std::os::raw::{c_int, c_char, c_uchar};
-use std::error;
-use std::fmt;
-use std::result;
-
-#[derive(Debug)]
-pub struct Error {
-    code: i32,
-    message: String,
-    class: i32
-}
-
-impl fmt::Display for Error {
-    fn fmt(&self, f: &mut fmt::Formatter) -> result::Result<(), fmt::Error> {
-        self.message.fmt(f)
-    }
-}
-
-impl error::Error for Error {
-    fn description(&self) -> &str { &self.message }
-}
-
-pub type Result<T> = result::Result<T, Error>;
 
 #[link(name = "git2")]
 extern {
