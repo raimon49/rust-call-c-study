f85e72f811a6a58fab76cbfb99f4463b275f46be
raimon <raimon49@hotmail.com>
2023-02-26 18:04:29 +0900

    Set blank line

--- src/main.rs
+++ src/main.rs
@@ -32,12 +32,16 @@ fn main() {
     {
         let path = std::env::args().skip(1).next()
             .expect("usage: git-toy PATH");
+
         let repo = git::Repository::open(&path)
             .expect("opening repository");
+
         let commit_oid = repo.reference_name_to_id("HEAD")
             .expect("looking up 'HEAD' reference");
+
         let commit = repo.find_commit(&commit_oid)
             .expect("looking up commit");
+
         let author = commit.author();
         println!("{} <{}>\n",
                  author.name().unwrap_or("(none)"),
